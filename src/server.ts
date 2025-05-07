e69719610e053b3914c5e2be931681cee140225d
ics-yash <yash.prasad@iqbsys.com>
2025-05-07 12:04:46 +0530

    added connection to socket server

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,45 @@
 import { WebSocket } from "ws";
-
-var name = "", age = 0, gender = "M", condition = "", count = 0;
-
-function welcome() {
-  return "Hello! 👋 I’m HealthBot. I can help you book an appointment with a doctor. How can I assist you today?"
-}
-
-function askName() {
-  return "Okay, can I get your name please?"
-}
-
-function askAge() {
-  return `Okay ${name}, and how old are you?`;
-}
-
-function askGender() {
-  return `${name}, what is your gender? (M/F)`;
-}
-
-function askProblem() {
-  return `${name}, what health issue are coming to get checked up on?`
-}
-
-function confirmAppointment() {
-  return `An appointment for ${name}, ${age}${gender} is booked with Dr. Aman Kumar for today @ 11:30 AM regarding: ${condition}`
-}
-
-var functionArr = [welcome, askName, askAge, askGender, askProblem, confirmAppointment];
-var size = functionArr.length;
+import net from "net";
 
 // Directly start a WebSocket server on port 4000
 const wss = new WebSocket.Server({ port: 4000 });
 
+const host = '192.168.0.215';
+const port = 65432;
+
 wss.on("connection", (ws) => {
-  ws.send(`${functionArr[count%size]()}`)
+  console.log("Connected to websocket through client");
+
+  const tcpClient = net.createConnection({ host, port }, () => {
+    console.log(`Connected to ${host}:${port}`)
+  });
+
+  tcpClient.on('data', (dataFromC: any) => {
+    console.log('Data from Raw Socket Server:', dataFromC.toString());
+    ws.send(dataFromC.toString());  // Or handle data format if needed
+  });
+  
+  tcpClient.on('connectionAttempt', () => {
+    console.log(`Attempted to connect to ${host}`);
+  });
+  tcpClient.on('connectionAttemptFailed', () => {
+    console.log(`Connection attempt to ${host} failed`);
+  });
+  tcpClient.on('connectionAttemptTimeout', () => {
+    console.log(`Connection attempt to ${host} timed out`);
+  });
 
   ws.on("message", (message) => {
-    console.log(message);
-    if(count % size == 1) {
-      name = message.toString();
-    }
-    if(count % size == 2) age = parseInt(message.toString());
-    if(count % size == 3) gender = message.toString();
-    if(count % size == 4) condition = message.toString();
-    count++;
-    ws.send(`${functionArr[count%size]()}`);
+    console.log(`Sent ${message} to ${host}:${port}`)
+    
+    tcpClient.write(message.toString());
   });
+
+  tcpClient.on('end', () => {
+    console.log(`TCP connection to ${host} closed`);
+    ws.close();
+  });
+
 });
 
 console.log("WebSocket-only server listening on port 4000");
